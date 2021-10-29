b5f9a4a2d82baf6a4a386fc2d32a3251b4491c50
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-10-29 14:38:57 +0100

    carousel progress

--- app/js/carousel.js
+++ app/js/carousel.js
@@ -1,3 +1,11 @@
+const slides = document.getElementsByClassName("mySlides");
+const currentImg = document.getElementById("currentImg");
+document.getElementById("totalImg").innerHTML = slides.length;
+document.documentElement.style.setProperty(
+  "--no-of-carousel-image",
+  slides.length
+);
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -17,8 +25,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -29,4 +36,9 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
+  currentImg.innerHTML = slideIndex;
+  document.documentElement.style.setProperty(
+    "--current-carousel-image",
+    slideIndex
+  );
 }
